56007e251187000d8afe65a32cfec29f2543de25
iliavetchinin <ivetchinin@gmail.com>
2021-03-01 22:24:16 +0300

    feat: pre-commit

--- homework1/tests/task3.py
+++ homework1/tests/task3.py
@@ -4,14 +4,12 @@ from homework1.task03 import find_maximum_and_minimum
 
 
 @pytest.mark.parametrize(
-    ['value', 'expected_result'],
+    ["value", "expected_result"],
     [
-        ('some_file.txt', (1, 5)),
+        ("some_file.txt", (1, 5)),
     ],
 )
 def test_find_maximum_and_minimum(value: str, expected_result: tuple):
     actual_result = find_maximum_and_minimum(value)
 
     assert actual_result == expected_result
-
-
