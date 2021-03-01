8ebdce67665e33c8af3dad6bc8f3e684c851c1fb
iliavetchinin <ivetchinin@gmail.com>
2021-03-01 20:59:41 +0300

    feat: new branch & step 4,5 done

---
+++ homework1/tests/task3.py
@@ -0,0 +1,17 @@
+import pytest
+
+from homework1.task03 import find_maximum_and_minimum
+
+
+@pytest.mark.parametrize(
+    ['value', 'expected_result'],
+    [
+        ('some_file.txt', (1, 5)),
+    ],
+)
+def test_find_maximum_and_minimum(value: str, expected_result: tuple):
+    actual_result = find_maximum_and_minimum(value)
+
+    assert actual_result == expected_result
+
+
