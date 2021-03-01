eb971e8b93daf3aa5aa90196df96efa9b1e0b9ee
iliavetchinin <ivetchinin@gmail.com>
2021-03-02 01:22:39 +0300

    fix: calc, task 03 fix: test 02, 04

--- homework1/tests/task02.py
+++ homework1/tests/task02.py
@@ -1,3 +1,15 @@
 import pytest
 
 from homework1.task02 import check_fibonacci
+
+
+@pytest.mark.parametrize(
+    ["value", "expected_result"],
+    [
+        ([1, 1, 2, 3, 5, 8], True),
+        ([1, 1, 2, 3, 5, 7], False)
+    ],
+)
+def test_chek_fibonacci(value: list, expected_result: bool):
+    actual_result = check_fibonacci(value)
+    assert expected_result == actual_result
---
+++ homework1/tests/task04.py
@@ -0,0 +1,14 @@
+import pytest
+
+from homework1.task04 import check_sum_of_four
+
+
+@pytest.mark.parametrize(
+    ["value", "value1", "value2", "value3", "expected_result"],
+    [
+        ([-20, -30], [20, 30], [-100, -200], [100, 200], 4),
+    ],
+)
+def test_chek_sum_of_for(value: list, value1: list, value2: list, value3: list, expected_result: int):
+    actual_result = check_sum_of_four(value, value1, value2, value3)
+    assert actual_result == expected_result
